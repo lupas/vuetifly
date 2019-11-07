60cb50d5f7e0b50fe462c40eab7dd7c95ce6f5f3
Pascal <pascal@luther.ch>
2019-11-07 17:57:20 +0800

    small fic

--- src/components/FlyAlert/mixin.js
+++ src/components/FlyAlert/mixin.js
@@ -1,6 +1,6 @@
 export default {
   methods: {
-    showFlyAlert({ color = 'success', text, defaultError = false }) {
+    showFlyAlert({ color = 'success', text = '', defaultError = false }) {
       if (defaultError) {
         color = 'error'
         text = 'Something went wrong, please try again.'
