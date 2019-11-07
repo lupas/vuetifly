4c5ce6d366514a988d83fe7ede3fc2d6229b6514
Pascal <pascal@luther.ch>
2019-11-07 17:08:28 +0800

    updated default error  to be error color

--- src/components/FlyAlert/mixin.js
+++ src/components/FlyAlert/mixin.js
@@ -2,6 +2,7 @@ export default {
   methods: {
     showFlyAlert({ color = 'success', text, defaultError = false }) {
       if (defaultError) {
+        color = 'error'
         text = 'Something went wrong, please try again.'
       }
       this.$store.commit('FlyAlertMutation', {
